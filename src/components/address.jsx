f1f447d828b1dcfac1682d2142cd10074159a494
Humayun Raza <humayunraza5@gmail.com>
2023-07-12 00:20:59 +0500

    fixed server

--- src/components/address.jsx
+++ src/components/address.jsx
@@ -72,7 +72,7 @@ function Address(props) {
         setSuccess(false);
         setLoading(true);
       }
-      const res = await fetch("http://karigarpk-server.onrender.com/order", {
+      const res = await fetch("https://karigarpk-server.onrender.com/order", {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
